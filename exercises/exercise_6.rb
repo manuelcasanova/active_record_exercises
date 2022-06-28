5acaedc683ceb98c78a37e3df6a5a8f374736258
Manuel Casanova <manucasanova@hotmail.com>
2022-06-28 07:49:49 -0700

    Add employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Manuel", last_name: "Casanova", hourly_rate: 18)
+@store1.employees.create(first_name: "Laura", last_name: "Hernandez", hourly_rate: 18)
+@store1.employees.create(first_name: "Aurora", last_name: "Fernandez", hourly_rate: 20)
+@store2.employees.create(first_name: "Vanesa", last_name: "Casanova", hourly_rate: 20)
+@store2.employees.create(first_name: "Luis", last_name: "Smith", hourly_rate: 5)
+@store2.employees.create(first_name: "Kathleen", last_name: "Turner", hourly_rate: 100)
