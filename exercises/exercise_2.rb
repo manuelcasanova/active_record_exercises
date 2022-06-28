76165aea63f78f2dedb60299270a0f8bffa0182f
Manuel Casanova <manucasanova@hotmail.com>
2022-06-28 06:30:25 -0700

    Load and assign stores. Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+
+@store1 = Store.find_by(id: 2)
+
+@store1.update(name: "North Vancouver")
+
+pp @store1
+pp @store2
