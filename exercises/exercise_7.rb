e35b6bf1a9cfe0ea595c12e504f2f603f9f0d38b
Manuel Casanova <manucasanova@hotmail.com>
2022-06-29 08:01:18 -0700

    Apply changes to test when errors and no errors

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -22,13 +22,6 @@ class Store < ActiveRecord::Base
       errors.add(:mens_apparel, "You cannot create a store without men's or women's apparel")
     end
   end
-
-  # def must_have_revenue
-  #   if annual_revenue != integer
-  #     errors.add(:mens_apparel, "You cannot create a store without an integer as annual_revenue")
-  #   end
-  # end
-
 end
 
 class Employee < ActiveRecord::Base
@@ -39,12 +32,9 @@ end
 print 'Add a store name '
 answer = gets.chomp.to_s
 
-# new_store = Store.create(name: answer)
-new_store = Store.create(name: answer, annual_revenue: 10, mens_apparel: true)
+new_store = Store.create(name: answer)
+# new_store = Store.create(name: answer, annual_revenue: 10, mens_apparel: true)
 
-# pp "Is new store valid? " + new_store.valid?.to_s.upcase
+
 pp "It is not valid because: "
-# pp "name errors: " + new_store.errors.details[:name].to_s
-# pp "annual_revenue errors: " + new_store.errors.details[:annual_revenue].to_s
-# pp "mens_apparel / womens_apparel errors: " + new_store.errors.details[:mens_apparel].to_s
 p new_store.errors.full_messages
